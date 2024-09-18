a4b0f518f38215a0b8b93fdbca56818728b6d893
marriamdd <davitashvili.m3@gmail.com>
2024-09-18 22:07:08 +0400

    style:listings container

--- src/components/ListItemPattern.jsx
+++ src/components/ListItemPattern.jsx
@@ -21,6 +21,12 @@ export default function ActionAreaCard() {
             sx={{ width: 385, marginBottom: 2, height: 455 }}
           >
             <CardActionArea>
+              <div
+                style={{ letterSpacing: "0.48px" }}
+                className="absolute top-[23px] left-[23px] p-[6px] rounded-[15px] text-[white] font-[500] text-[12px] bg-[#02152680]"
+              >
+                {listing.is_rental ? "ქირავდება" : "იყიდება"}
+              </div>
               <CardMedia
                 component="img"
                 height="307" // Updated height to 307px
@@ -33,26 +39,38 @@ export default function ActionAreaCard() {
               />
               <CardContent>
                 <Typography
-                  className="text-[28px] text-[#021526] font-700"
+                  className="text-[28px] pl-[15px]  text-[#021526] font-700"
                   gutterBottom
                   variant="h5"
                   component="div"
                 >
                   {formatPrice(listing.price)}
                 </Typography>
-                <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                  {listing.address || "No address available"}
-                </Typography>
-                <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                  Price:{" "}
-                  {listing.price ? `$${listing.price}` : "No price available"}
-                </Typography>
-                <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                  Bedrooms: {listing.bedrooms}
-                </Typography>
-                <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                  Area: {listing.area} sq. ft.
-                </Typography>
+                <div className="flex flex-col gap-[10px] pl-[15px] text-[16px] text-[#021526B2]">
+                  <div className="flex text-[#021526B2]">
+                    <img src="/location-marker.svg" alt="" />{" "}
+                    {listing.city.name} , {listing.address}
+                  </div>
+
+                  <div className="flex items-center gap-[32px]">
+                    <div className="flex gap-[5px] ">
+                      <img src="/bed.svg" alt="" />{" "}
+                      <span className="text-[16px] text-[#021526B2]">
+                        {" "}
+                        {listing.bedrooms}
+                      </span>
+                    </div>
+
+                    <div className="flex gap-[5px] ">
+                      <img src="/Vector (6).svg" alt="" />
+                      <span>{listing.area}</span> <span>მ ²</span>
+                    </div>
+                    <div className="flex gap-[5px] ">
+                      <img src="/Vector (7).svg" alt="" />
+                      <span>{listing.zip_code}</span>
+                    </div>
+                  </div>
+                </div>
               </CardContent>
             </CardActionArea>
           </Card>
